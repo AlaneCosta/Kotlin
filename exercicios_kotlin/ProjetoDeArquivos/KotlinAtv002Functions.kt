98d14a62741130a5b06b54112f61147c26aceb12
Alane Costa <134459619+AlaneCosta@users.noreply.github.com>
2023-12-10 19:05:02 -0300

    Funções básicas
    Verificando funcões básicas

--- exercicios_kotlin/ProjetoDeArquivos/KotlinAtv002Functions.kt
+++ exercicios_kotlin/ProjetoDeArquivos/KotlinAtv002Functions.kt
@@ -1,3 +1,22 @@
+fun printMessage(message: String): Unit {                               // 1
+    println(message)
+}
+
+fun printMessageWithPrefix(message: String, prefix: String = "Info") {  // 2
+    println("[$prefix] $message")
+}
+
+fun sum(x: Int, y: Int): Int {                                          // 3
+    return x + y
+}
+
+fun multiply(x: Int, y: Int) = x * y                                    // 4
+
 fun main() {
-    println("Hello, world!!!")
+    printMessage("Alane no Kotlin")                                               // 5                    
+    printMessageWithPrefix("Mensagem", "Prefixo")                              // 6
+    printMessageWithPrefix("Sem passar prefixo")                                     // 7
+    printMessageWithPrefix(prefix = "Prefixo", message = "Prefixo passado antes")           // 8
+    println(sum(5, 9))                                                  // 9
+    println(multiply(7, 4))                                             // 10
 }
